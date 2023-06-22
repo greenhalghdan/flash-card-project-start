508486b37f9607b8d1bd5d59df18df9a9a974ae6
danie <greenhalghdan@gmail.com>
2023-06-22 16:27:55 +0100

    Create a progress file, remove correct guesses from the file

--- main.py
+++ main.py
@@ -8,27 +8,36 @@ from pandas import *
 from random import *
 
 def correct():
-    global current_word
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
+
+    data = read_csv("data/wordtolearn.csv")
+    data = data.loc[data["French"] != current_word["French"]]
+    data.to_csv("data/wordtolearn.csv", index=False)
     current_word = new_word()
     canvas.itemconfig(word, text=current_word["French"], fil="black")
     canvas.itemconfig(img, image=cardf_img)
     canvas.itemconfig(title, text="French", fill="black")
-    window.after(3000, updatecanvas)
+    flip_timer = window.after(3000, updatecanvas)
 def wrongdef():
-    global current_word
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
     current_word = new_word()
     canvas.itemconfig(word, text=current_word["French"], fill="black")
     canvas.itemconfig(img, image=cardf_img)
     canvas.itemconfig(title, text="French", fill="black")
-    window.after(3000, updatecanvas)
+    flip_timer = window.after(3000, updatecanvas)
 
 #picking random words
 def new_word():
-    data = read_csv("data/french_words.csv")
-    dict123 = choice(data.to_dict(orient="records"))
-    English_word = dict123["English"]
-    french_word = dict123["French"]
-
+    try:
+        data = read_csv("data/wordtolearn.csv")
+    except FileNotFoundError:
+        df = read_csv("data/french_words.csv")
+        df.to_csv("data/wordtolearn.csv", index=False)
+    finally:
+        data = read_csv("data/wordtolearn.csv")
+        dict123 = choice(data.to_dict(orient="records"))
     return dict123
 
 def updatecanvas():
@@ -63,6 +72,6 @@ right = PhotoImage(file="images/right.png")
 button2 = Button(image=right, highlightthickness=0, command=correct)
 button2.grid(column=2, row=2)
 
-window.after(3000, updatecanvas)
+flip_timer = window.after(3000, updatecanvas)
 
 window.mainloop()
